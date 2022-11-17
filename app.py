4dd3edef9bb0efc13b9af21708c1a2e722d465cc
laynejohnson13 <laynejohnson13001@gmail.com>
2022-11-17 11:52:28 -0500

    app.py edit

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
 import pandas as pd
 import streamlit as st
+
+##importing data
+slp = pd.read_csv("data/slp_small.csv")
+slp
+
+##creating title
+st.title('Job Satisfaction of Speech Language Pathologists ')
+
+#creating caption
+st.caption('collected via Qualtrics Survey')
+
+if st.checkbox('Survey Responses'):
+    st.dataframe(slp)
+
+##barchart
+st.subheader('Length of Employment as a SLP')
+employment_length = slp['How_long_have_you_been_working_as_a_Speech_Language_Pathologist_'].value_counts()
+st.bar_chart(employment_length)
+st.caption('This visual gives us an idea as to length of employment per survey response')
